3874a67520bcc766e4c5e616a5261b38078ef0dc
keanen3408 <debruijn2008@gmail.com>
2026-03-16 14:07:14 +0100

    Update db.php

--- bibliotheek/db.php
+++ bibliotheek/db.php
@@ -1,16 +1,1 @@
-<?php
 
-$host = "localhost";
-$dbname = "bibliotheek";
-$user = "root";
-$password = "";
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} 
-catch(PDOException $e) {
-    die("Database fout: " . $e->getMessage());
-}
-
-?>
